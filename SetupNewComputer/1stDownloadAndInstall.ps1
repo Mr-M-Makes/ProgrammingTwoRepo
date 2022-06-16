97c8fd3e2992f15e97d45c8f5c50509f39d8532d
Mr-M-Makes <bmatthews@fortsmithschools.org>
2022-06-15 22:14:59 -0500

    Installer hopefully done

--- SetupNewComputer/1stDownloadAndInstall.ps1
+++ SetupNewComputer/1stDownloadAndInstall.ps1
@@ -1,3 +1,4 @@
+
 #$UserPath = "$($env:USERPROFILE)\Desktop\InstallArduino.exe"
 $arduino = "https://downloads.arduino.cc/arduino-1.8.19-windows.exe"
 
@@ -5,11 +6,16 @@ $arduino = "https://downloads.arduino.cc/arduino-1.8.19-windows.exe"
 #Download the file
 Invoke-WebRequest -Uri $arduino -OutFile "..\SetupNewComputer\Includes\ard.exe"
 
-$proc = Start-Process "..\SetupNewComputer\Includes\Pystall.exe" -NoNewWindow -PassThru
-$proc.WaitForExit()
-$proc = Start-Process "..\SetupNewComputer\Includes\VSC.exe" -NoNewWindow -PassThru
-$proc.WaitForExit()
-$proc = Start-Process "..\SetupNewComputer\Includes\ard.exe" -NoNewWindow -PassThru
-$proc.WaitForExit()
-Remove-Item '"..\SetupNewComputer\Includes\ard.exe"'
-echo "done"
+$proc = Start-Process "..\SetupNewComputer\Deprecated_1st_run_in_Cmd.bat" -NoNewWindow -PassThru
+
+# $proc = Start-Process "..\SetupNewComputer\Includes\Pystall.exe" -NoNewWindow -PassThru
+# $proc.WaitForExit()
+# $proc = Start-Process "..\SetupNewComputer\Includes\VSC.exe" -NoNewWindow -PassThru
+# $proc.WaitForExit()
+# Write-Output "Timeout is for 10 seconds"
+# Timeout /T 10
+# Write-Output "This line will be executed after 10 seconds if not interuptted"
+# $proc = Start-Process "..\SetupNewComputer\Includes\ard.exe" -NoNewWindow -PassThru
+# $proc.WaitForExit()
+# Remove-Item '"..Includes\ard.exe"'
+# echo "done"
