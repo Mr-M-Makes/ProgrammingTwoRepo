705c8362ec430da20bac34cc2e33d3c0acfb4fcd
Mr-M-Makes <bmatthews@fortsmithschools.org>
2022-06-21 16:31:18 -0500

    Revert "inheritance"
    This reverts commit 28f08cacb94f675d84d52d7c372f3799f69a3c0d.

--- Examples/Python/CallOtherPyFile/ImportMyLibrary.py
+++ Examples/Python/CallOtherPyFile/ImportMyLibrary.py
@@ -1,4 +1,17 @@
+<<<<<<< HEAD
+
 import MyObjInDict as f
+
+
+=======
+from types import new_class
+import MyObjInDict as f
+
+#This class inherits everything about the parent class object
+#The class "object" is found in the MyObjInDict.py that we imported
+Class new_class_hi(object)
+    pass
+>>>>>>> 28f08cacb94f675d84d52d7c372f3799f69a3c0d
 
 f.new_object()
 f.Menu()
