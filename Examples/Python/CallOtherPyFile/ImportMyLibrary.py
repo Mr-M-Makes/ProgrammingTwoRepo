28f08cacb94f675d84d52d7c372f3799f69a3c0d
Mr-M-Makes <bmatthews@fortsmithschools.org>
2022-06-21 15:55:57 -0500

    inheritance

--- Examples/Python/CallOtherPyFile/ImportMyLibrary.py
+++ Examples/Python/CallOtherPyFile/ImportMyLibrary.py
@@ -1,10 +1,10 @@
 from types import new_class
 import MyObjInDict as f
 
+#This class inherits everything about the parent class object
+#The class "object" is found in the MyObjInDict.py that we imported
 Class new_class_hi(object)
     pass
-    #This class inherits everything about the parent class object
-    #The class "object" is found in the MyObjInDict.py that we imported
 
 f.new_object()
 f.Menu()
